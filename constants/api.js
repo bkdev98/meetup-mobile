e903614c882e3827c372adb0a028bf0c9f905d5e
bkdev98 <bkdev98@gmail.com>
2017-04-02 09:54:24 +0700

    Add meetup cards with nice styles

--- constants/api.js
+++ constants/api.js
@@ -7,7 +7,7 @@ const fakeGroupId = '58dca7a5b1d9174b8302859c';
 class MeetupApi {
   constructor() {
     this.groupId = fakeGroupId;
-    this.path = `/groups/${this.group}/meetups`;
+    this.path = `/groups/${this.groupId}/meetups`;
   }
 
   async fetchGroupMeetups() {
--- main.js
+++ main.js
@@ -1,13 +1,47 @@
-import Expo from 'expo';
+import Expo, { AppLoading } from 'expo';
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Colors from './constants/Colors';
 import { HomeScreen } from './src/screens';
+import { cachedFonts } from './helpers';
 
 EStyleSheet.build(Colors);
 
 class App extends React.Component {
+  state = {
+    fontLoaded: false
+  }
+
+  componentDidMount() {
+    this._loadAssetsAsync();
+  }
+
+  async _loadAssetsAsync() {
+    const fontAssets = cachedFonts([
+      {
+        montserrat: require('./assets/fonts/Montserrat-Regular.ttf')
+      },
+      {
+        montserratBold: require('./assets/fonts/Montserrat-Bold.ttf')
+      },
+      {
+        montserratLight: require('./assets/fonts/Montserrat-Light.ttf')
+      },
+      {
+        montserratMedium: require('./assets/fonts/Montserrat-Medium.ttf')
+      }
+    ]);
+
+    await Promise.all(fontAssets);
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
+    if (!this.state.fontLoaded) {
+      return <AppLoading />;
+    }
+
     return <HomeScreen />;
   }
 }
