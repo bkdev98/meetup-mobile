f6d27e317c0b7d174367fcdd8f41073c90846ade
bkdev98 <bkdev98@gmail.com>
2017-04-06 08:55:23 +0700

    play with styled-component and flexbox for first auth screen

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
@@ -11,7 +11,7 @@ const middlewares = [
 ];
 
 if (__DEV__) {//  eslint-disable-line
-  middlewares.push(logger);
+  middlewares.push(createLogger());
 }
 
 const enhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
