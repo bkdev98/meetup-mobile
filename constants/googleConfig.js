8534a1699515b9c35c61fc335c34b849acf5274e
bkdev98 <bkdev98@gmail.com>
2017-05-09 23:20:00 +0700

    add auth with google provider

---
+++ constants/googleConfig.js
@@ -0,0 +1,3 @@
+export default {
+  CLIENT_ID_IOS: '571110615530-ole6bjm0m26ke46ho5ep84poisc703rh.apps.googleusercontent.com',
+};
--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Facebook } from 'expo';
-import { Text } from 'react-native';
+import { Facebook, Google } from 'expo';
+import { Text, Alert } from 'react-native';
 import styled from 'styled-components/native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Fonts from '../../../constants/Fonts';
 import Colors from '../../../constants/Colors';
 import fbConfig from '../../../constants/fbConfig';
+import googleConfig from '../../../constants/googleConfig';
 
 const MeetupText = styled.Text`
   color: ${Colors.redColor};
@@ -47,12 +48,39 @@ export default class LoginScreen extends Component {
   }
 
   async _logInWithFacebook() {
-    const { type, token } = await Facebook.logInWithReadPermissionsAsync(fbConfig.APP_ID, {
-      permissions: ['public_profile', 'email'],
-    });
+    try {
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync(fbConfig.APP_ID, {
+        permissions: ['public_profile', 'email'],
+      });
+
+      if (token) {
+        Alert.alert('Logged with facebook');
+      }
+    } catch (e) {
+      throw e;
+    }
 
     if (type === 'success') {
       const resp = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+    }
+  }
+
+  async _loginWithGoogle() {
+    try {
+      const result = await Google.logInAsync({
+        iosClientId: googleConfig.CLIENT_ID_IOS,
+        scopes: ['profile', 'email'],
+      });
+
+      if (result.type === 'success') {
+        Alert.alert(`Logged with google, ${result.accessToken}`);
+      } else {
+        return {
+          cancelled: true
+        };
+      }
+    } catch (e) {
+      throw e;
     }
   }
 
